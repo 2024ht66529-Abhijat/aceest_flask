840041322150469215fd49a33eee1f158e6d5a3e
2024ht66529-Abhijat <2024ht66529@wilp.bits-pilani.ac.in>
2026-04-03 23:23:12 +0530

    Inital commit

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import pytest
+from app import create_app
+
+def test_home_page():
+    app = create_app()
+    app.testing = True
+    client = app.test_client()
+    res = client.get('/')
+    assert res.status_code == 200
